a2b12815256a0a3c2c87a0e68a12f7e73b92c2cd
Joan Martinez <joan.fontanals.martinez@jina.ai>
2025-03-20 11:36:03 +0100

    ci: skip tests with specific port

--- tests/unit/orchestrate/flow/flow-construct/test_flow_yaml_parser.py
+++ tests/unit/orchestrate/flow/flow-construct/test_flow_yaml_parser.py
@@ -109,13 +109,14 @@ def test_dump_load_build(monkeypatch):
     # validate gateway args (set during build)
     assert f['gateway'].args.port == f2['gateway'].args.port
 
-
+@pytest.mark.skipif('GITHUB_WORKFLOW' in os.env, reason='no specific port test in CI')
 def test_load_flow_with_port():
     f = Flow.load_config('yaml/test-flow-port.yml')
     with f:
         assert f.port == 12345
 
 
+@pytest.mark.skipif('GITHUB_WORKFLOW' in os.env, reason='no specific port test in CI')
 def test_load_flow_from_cli():
     a = set_flow_parser().parse_args(['--uses', 'yaml/test-flow-port.yml'])
     f = Flow.load_config(a.uses)
